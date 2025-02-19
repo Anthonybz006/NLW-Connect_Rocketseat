3ba441d3bc0aa70368ab9c74b41ea31a990c9480
Anthonybz006 <antoniozago186@gmail.com>
2025-02-18 23:05:42 -0300

    Update index.html
    Get the value from inputs of html tags

--- Full-Stack/src/scripts/index.js
+++ Full-Stack/src/scripts/index.js
@@ -1,5 +1,23 @@
 const app = document.querySelector('#app')
 
+const users = [
+
+    {
+        id: 0,
+        email: 'teste@teste.com.br',
+        phone: '22 999999999',
+        refID: null
+    },
+
+    {
+        id: 1,
+        email: 'user1@nlwconnect.com.br',
+        phone: '21 997584517',
+        refID: 0
+    },
+
+]
+
 // Função para resetar a configuração original
 const formActions = () => {
     const form = document.querySelector('#form')
@@ -7,6 +25,15 @@ const formActions = () => {
     // Chamada quando o botão submit for clicado
     form.onsubmit = (event) => {
         event.preventDefault()
+
+        // Coleta todos os valores dos inputs, pelo nome de cada elemento
+        const formData = new FormData(form)
+
+        const userData = {
+
+            email: formData.get('email'),
+            phone: formData.get('phone')
+        }
     }
 }
 
@@ -17,7 +44,8 @@ const startApp = () => {
     const content = `
         <form id="form">
             <input type="email" name="email" id="e-mail_input" class="e-mail" placeholder="E-mail">
-            <input type="tel" name="tel" id="tel_input" class="tel" placeholder="Telefone">
+            
+            <input type="tel" name="phone" id="tel_input" class="tel" placeholder="Telefone">
 
             <button type="submit" class="convirmar" id="btn-confirmar">Confirmar</button>
         </form>
